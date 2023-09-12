d3f2f857f8ff1053c00ea8dff1c2a0b69d2147cd
sergioshtromb <81059697+sergashtromb@users.noreply.github.com>
2023-09-12 19:26:11 +0700

    Доработки визуала

--- frontend/to-do-front/src/Components/Content.jsx
+++ frontend/to-do-front/src/Components/Content.jsx
@@ -9,16 +9,28 @@ function Content() {
     let task_list: TypeTask[] = [
         {author_id: 0, id: 0, text: 'Погулять', is_done: true, start_date: new Date(), end_date: new Date("0001-01-01"), notify_date: new Date("0001-01-01")},
         {author_id: 0, id: 1, text: 'Почитать', is_done: true, start_date: new Date(), end_date: new Date("0001-01-01"), notify_date: new Date("0001-01-01")},
-        {author_id: 0, id: 2, text: 'Сходить в магазин', is_done: false, start_date: new Date(), end_date: new Date("0001-01-01"), notify_date: new Date("0001-01-01")}
+        {author_id: 0, id: 2, text: 'Сходить в магазин', is_done: false, start_date: new Date(), end_date: new Date("0001-01-01"), notify_date: new Date("0001-01-01")},
+        {author_id: 0, id: 2, text: 'Сходить в магазин', is_done: false, start_date: new Date(), end_date: new Date("0001-01-01"), notify_date: new Date("0001-01-01")},
+        {author_id: 0, id: 2, text: 'Сходить в магазин', is_done: false, start_date: new Date(), end_date: new Date("0001-01-01"), notify_date: new Date("0001-01-01")},
+        {author_id: 0, id: 2, text: 'Сходить в магазин', is_done: false, start_date: new Date(), end_date: new Date("0001-01-01"), notify_date: new Date("0001-01-01")},
+        {author_id: 0, id: 2, text: 'Сходить в магазин', is_done: false, start_date: new Date(), end_date: new Date("0001-01-01"), notify_date: new Date("0001-01-01")},
+        {author_id: 0, id: 2, text: 'Сходить в магазин', is_done: false, start_date: new Date(), end_date: new Date("0001-01-01"), notify_date: new Date("0001-01-01")},
+        {author_id: 0, id: 2, text: 'Сходить в магазин', is_done: false, start_date: new Date(), end_date: new Date("0001-01-01"), notify_date: new Date("0001-01-01")},
+        {author_id: 0, id: 2, text: 'Сходить в магазин', is_done: false, start_date: new Date(), end_date: new Date("0001-01-01"), notify_date: new Date("0001-01-01")},
+        {author_id: 0, id: 2, text: 'Сходить в магазин', is_done: false, start_date: new Date(), end_date: new Date("0001-01-01"), notify_date: new Date("0001-01-01")},
     ];
 
     return (
         <div id="content">
-            <ul id="task_list">
+
+            <div id="task_list">
+
                 {task_list.map((task) => (
-                    <li><Task data={task}/></li>
+                    <Task data={task}/>
                 ))}
-            </ul>
+                
+            </div>
+
         </div>
     );
 }
